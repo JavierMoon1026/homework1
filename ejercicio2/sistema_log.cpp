654e0be784c8f63ed5c21ea2765284dc380ebc23
JavierMoon1026 <moonj@udesa.edu.ar>
2025-03-21 04:21:42 +0000

    se siguio con el ejercicio2

---
+++ ejercicio2/sistema_log.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+enum class NivelSeveridad{DEBUG, INFO, WARNING, ERROR, CRITICAL, SECURITY};
+
+void logMessage(string mensaje, NivelSeveridad nivel){
+    ofstream logFile("log.txt", ios::app);
+    if(logFile.is_open()){
+        switch (nivel){
+        case NivelSeveridad::DEBUG:
+            logFile << "[DEBUG] " << mensaje << endl; break;
+        case NivelSeveridad::INFO:
+            logFile << "[INFO] " << mensaje << endl; break;
+        case NivelSeveridad::WARNING:
+            logFile << "[WARNING] " << mensaje << endl; break;
+        case NivelSeveridad::ERROR:
+            logFile << "[ERROR] " << mensaje << endl; break;
+        case NivelSeveridad::CRITICAL:
+            logFile << "[CRITICAL] " << mensaje << endl; break;
+        case NivelSeveridad::SECURITY:
+            logFile << "[SECURITY] " << mensaje << endl; break;
+        }
+        logFile.close();
+    } else
+        cerr << "error abriendo el archivo!" << endl;
+}
+
+void logMessage(string mensaje, string archivo, int linea){
+    string mensaje_completo = mensaje + " (Archivo: " + archivo + ", Línea: " + to_string(linea) + ")";
+    logMessage(mensaje_completo, NivelSeveridad::ERROR);
+}
+
+void logMessage(string mensaje, string username){
+    string mensaje_completo = mensaje +  mensaje + " Usuario: " + username;
+    logMessage(mensaje_completo, NivelSeveridad::SECURITY);
+}
+
+int main(){
+    logMessage("Inicio del sistema", NivelSeveridad::DEBUG);
+    logMessage("Inicio del sistema", NivelSeveridad::INFO);
+    logMessage("Inicio del sistema", NivelSeveridad::WARNING);
+    logMessage("Inicio del sistema", NivelSeveridad::ERROR);
+    logMessage("Inicio del sistema", NivelSeveridad::CRITICAL);
+
+    return 0;
+}
